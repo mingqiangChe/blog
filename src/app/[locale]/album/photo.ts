9414396c112b7e20847e6be25757d522b11a25dd
unknown <thomaschefowshu@gmail.com>
2025-06-19 17:57:08 +0800

    feat:描述修改

--- src/app/[locale]/album/photo.ts
+++ src/app/[locale]/album/photo.ts
@@ -8,57 +8,47 @@ export const photos: Photo[] = [
   {
     url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04445.jpg',
     desc: '这里是大理环洱海最美的一段公路边的风景 我和在租机车认识的老哥一起环洱海路上拍摄的 用他的索尼长焦镜头',
-    tip: '洱海',
-  },
-  {
-    url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04451.jpg',
-    desc: '暂时无',
-    tip: '洱海',
+    tip: '洱海环行',
   },
   {
     url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04450.jpg',
     desc: '暂时无',
-    tip: '洱海',
+    tip: '洱海环行',
   },
   {
     url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04444.jpg',
     desc: '暂时无',
-    tip: '洱海',
+    tip: '洱海环行',
   },
   {
     url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04452.jpg',
     desc: '暂时无',
-    tip: '洱海',
+    tip: '洱海环行',
   },
-  {
-    url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04443.jpg',
-    desc: '暂时无',
-    tip: '洱海',
-  },
+  // {
+  //   url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04443.jpg',
+  //   desc: '暂时无',
+  //   tip: '洱海环行',
+  // },
   {
     url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04442.jpg',
     desc: '暂时无',
-    tip: '洱海',
+    tip: '洱海环行',
   },
-  {
-    url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04441.jpg',
-    desc: '暂时无',
-    tip: '洱海',
-  },
+  // {
+  //   url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04441.jpg',
+  //   desc: '暂时无',
+  //   tip: '洱海环行',
+  // },
   {
     url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04459.jpg',
     desc: '暂时无',
-    tip: '洱海',
+    tip: '洱海环行',
   },
   {
     url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04465.jpg',
     desc: '8778',
-    tip: '洱海',
-  },
-  {
-    url: 'https://chemingqiang.oss-cn-shenzhen.aliyuncs.com/img/%E6%9C%BA%E8%BD%A6_PixCake/DSC04465.jpg',
-    desc: '8778',
-    tip: '大理妹子',
+    tip: '洱海环行',
   },
   // ...更多图片
 ];
