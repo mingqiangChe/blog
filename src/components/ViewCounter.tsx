a3b15737a6074b2a1a51baf6a3b50ea9a551209f
Thomas che <cmq15650272396@163.com>
2025-07-16 22:23:44 +0800

    fix

--- src/components/ViewCounter.tsx
+++ src/components/ViewCounter.tsx
@@ -10,7 +10,7 @@ interface ViewCounterProps {
 // 初始化 Supabase 客户端
 const supabase = createClient(
   'https://fusrmsbzfmnicfsbyjzd.supabase.co',
-  '你的匿名Key替换这里'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1c3Jtc2J6Zm1uaWNmc2J5anpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2NTgwNDgsImV4cCI6MjA2ODIzNDA0OH0.xtmEdoPwsT_O8WqNnpvg-eHNst33O-ncc3B4rJ8MqUA'
 );
 
 export default function ViewCounter({ slug }: ViewCounterProps) {
