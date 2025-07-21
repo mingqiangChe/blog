78b15184f3a8879cbf6c432ec5d60f190189b66b
ThomasChe <thomaschefowshu@gmail.com>
2025-07-21 17:38:27 +0800

    fix：seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,13 @@ module.exports = {
   siteUrl: 'https://thomasche.top',
   generateRobotsTxt: true,
   outDir: './public',
+  additionalPaths: async (config) => [
+    { loc: '/' },
+    { loc: '/blog' },
+    { loc: '/search' },
+    { loc: '/media' },
+    { loc: '/album' },
+    { loc: '/milestone' },
+    { loc: '/about' },
+  ],
 };
